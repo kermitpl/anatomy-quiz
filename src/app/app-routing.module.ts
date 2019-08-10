329e0015ad967b08045420c1f95943a9985dd3cc
Adrian <jambondadri@gmail.com>
2019-08-10 10:51:12 +0200

    GithubPages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component';
+import {QuizComponent} from './quiz/quiz.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent}
+  {path: '', component: QuizComponent}
 ];
 
 @NgModule({
